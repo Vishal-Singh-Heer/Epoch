5603d7a49d5b156e6d6aed3ab388b9409b41addc
aymanhki <agamyahk@myumanitoba.ca>
2024-03-29 10:42:03 -0500

    Bug fix for feed scrolling

--- epoch_frontend/src/modules/Feed.js
+++ epoch_frontend/src/modules/Feed.js
@@ -35,7 +35,7 @@ export default function Feed({
     const toRemove = 15; // Must be smaller than maxPosts
     const [previousPosts, setPreviousPosts] = useState([]);
     const bottomElementRef = useRef(null);
-    // const [ref, inView] = useInView({threshold: 0.1, });
+    const [ref, inView] = useInView({threshold: 0.1, });
 
 
     const refreshFeedPosts =  (reset, fromTop) =>
