40f5a0ccb37468cb90e113f8f2a2eff4a4face4f
aymanhki <agamyahk@myumanitoba.ca>
2024-03-30 08:57:23 -0500

    Bug fix for feed scrolling

--- epoch_frontend/src/modules/Feed.js
+++ epoch_frontend/src/modules/Feed.js
@@ -105,17 +105,16 @@ export default function Feed({
         let finalToSetFeedPosts = finalFeedPosts;
 
 
-
-        // if (finalFeedPosts.length - toRemove > maxPosts)
-        // {
-        //     setPreviousPosts(previousPosts.concat(finalFeedPosts.slice(0, toRemove + (maxPosts - toRemove))));
-        //     finalToSetFeedPosts = finalToSetFeedPosts.slice(0, toRemove + (maxPosts - toRemove));
-        // }
-        // else
-        // {
+        if (finalFeedPosts.length - toRemove > maxPosts)
+        {
+            setPreviousPosts(previousPosts.concat(finalFeedPosts.slice(0, toRemove + (maxPosts - toRemove))));
+            finalToSetFeedPosts = finalToSetFeedPosts.slice(0, toRemove + (maxPosts - toRemove));
+        }
+        else
+        {
             setPreviousPosts(previousPosts.concat(finalFeedPosts.slice(0, toRemove)));
-            finalToSetFeedPosts = finalToSetFeedPosts.slice(toRemove);
-        // }
+            finalToSetFeedPosts = finalToSetFeedPosts.slice(0, toRemove);
+        }
 
         finalToSetFeedPosts = finalToSetFeedPosts.concat(data);
 
